cd379dd1d3254b1868fdfc0a039e3168587c4d24
Takafumi Arakaki <aka.tkf@gmail.com>
2020-10-11 19:13:37 -0700

    Run doctests (#9)

--- src/CUDAFolds.jl
+++ src/CUDAFolds.jl
@@ -23,4 +23,10 @@ using Transducers:
 include("kernels.jl")
 include("api.jl")
 
+# Use README as the docstring of the module:
+@doc let path = joinpath(dirname(@__DIR__), "README.md")
+    include_dependency(path)
+    replace(read(path, String), r"^```julia"m => "```jldoctest README")
+end CUDAFolds
+
 end
