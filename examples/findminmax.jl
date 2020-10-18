73a5fd8539f5d96bfc6466935382ce3bde22f00d
Takafumi Arakaki <aka.tkf@gmail.com>
2020-10-17 18:47:05 -0700

    A bit of demo in findminmax.jl

--- examples/findminmax.jl
+++ examples/findminmax.jl
@@ -1,4 +1,4 @@
-# `findminmax`
+# # `findminmax`
 
 using Transducers
 using CUDA
@@ -30,3 +30,15 @@ function findminmax_base(xs)
     xmax, imax = findmax(xs)
     return (; imin, xmin, imax, xmax)
 end
+nothing  # hide
+#-
+
+xs = [700, 900, 500, 200, 700, 700, 900, 300, 600, 400, 900, 600, 900, 800, 600]
+if has_cuda_gpu()
+    xs = CuArray(xs)
+end
+
+result = findminmax(xs)
+#-
+
+@assert result == findminmax_base(xs)
