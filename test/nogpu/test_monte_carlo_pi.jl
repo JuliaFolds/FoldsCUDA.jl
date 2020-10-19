d0ca9377a91fe6a44168beea4ba51c1eca2309a2
Takafumi Arakaki <aka.tkf@gmail.com>
2020-10-18 17:26:54 -0700

    Fix test_monte_carlo_pi.jl

--- test/nogpu/test_monte_carlo_pi.jl
+++ test/nogpu/test_monte_carlo_pi.jl
@@ -2,5 +2,5 @@ module TestMonteCarloPi
 using LiterateTest.AssertAsTest: @assert
 include("../../examples/monte_carlo_pi.jl")
 using Test
-@test (πₐₚₚᵣₒₓ - π) / π < 1e-2
+@test abs(πₐₚₚᵣₒₓ - π) / π < 1e-2
 end  # module
