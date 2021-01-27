81a1fb724e4b706d08e56298f3d90e94622a9bb2
Takafumi Arakaki <aka.tkf@gmail.com>
2021-01-26 17:23:34 -0800

    Stop using nogpu tag from Folds.Testing

--- test/test_with_sequential.jl
+++ test/test_with_sequential.jl
@@ -2,16 +2,32 @@ module TestWithSequential
 
 using Adapt
 using CUDA
-using Folds.Testing: test_with_sequential
+using Folds.Testing: parse_tests, test_with_sequential
 using FoldsCUDA
 
-function upload(x)
-    Base.isbitsunion(eltype(x)) && return nothing
-    return adapt(CuArray, x)
-end
+rawdata = """
+count(isodd(x) for x in 1:10)
+extrema((x - 5)^2 for x in 1:10)
+mapreduce(x -> x^2, +, 1:10)
+mapreduce(*, +, 1:10, 11:20)
+mapreduce(*, +, 1:10, 11:20, 21:30)
+maximum(0:9)
+maximum(9:-1:0)
+maximum([2, 3, 0, 3, 4, 0, 5, 7, 4, 2])
+minimum(0:9)
+minimum(9:-1:0)
+minimum([2, 3, 0, 3, 4, 0, 5, 7, 4, 2])
+prod(1:2:10)
+prod(y for x in 1:11 if isodd(x) for y in 1:x:x^2; init = 1)
+sum(1:10)
+sum(x^2 for x in 1:11)
+sum(x^2 for x in 1:11 if isodd(x); init = 0)
+"""
 
-include_test(test) = :nogpu ∉ test.tags
+tests = parse_tests(rawdata, @__MODULE__)
 
-test_with_sequential([CUDAEx()]; upload = upload, include_test = include_test)
+upload(x) = adapt(CuArray, x)
+
+test_with_sequential(tests, [CUDAEx()]; upload = upload)
 
 end  # module
