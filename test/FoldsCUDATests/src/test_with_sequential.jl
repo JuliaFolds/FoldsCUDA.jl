fe6581e4abb5cf025e5f3ad472411764d316ad79
Takafumi Arakaki <aka.tkf@gmail.com>
2021-04-16 21:55:25 -0400

    Test with TeeRF

--- test/FoldsCUDATests/src/test_with_sequential.jl
+++ test/FoldsCUDATests/src/test_with_sequential.jl
@@ -4,6 +4,7 @@ using Adapt
 using CUDA
 using Folds.Testing: parse_tests, test_with_sequential
 using FoldsCUDA
+using Transducers
 
 rawdata = """
 count(isodd(x) for x in 1:10)
@@ -19,6 +20,9 @@ minimum(9:-1:0)
 minimum([2, 3, 0, 3, 4, 0, 5, 7, 4, 2])
 prod(1:2:10)
 prod(y for x in 1:11 if isodd(x) for y in 1:x:x^2; init = 1)
+reduce(TeeRF(min, max), 1:10)
+reduce(TeeRF(min, max), (2x for x in 1:10))
+reduce(TeeRF(min, max), (2x for x in 1:10 if isodd(x)))
 sum(1:10)
 sum(x^2 for x in 1:11)
 sum(x^2 for x in 1:11 if isodd(x); init = 0)
