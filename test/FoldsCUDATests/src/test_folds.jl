4337af1258e500f0ab14df8cda695992d1219327
Takafumi Arakaki <aka.tkf@gmail.com>
2021-04-17 23:58:43 -0400

    Hard-code init for Julia < 1.6

--- test/FoldsCUDATests/src/test_folds.jl
+++ test/FoldsCUDATests/src/test_folds.jl
@@ -6,6 +6,8 @@ using Test
 
 function test_sum_pairs()
     xs = CUDA.rand(Int32, 100)
+    @test Folds.sum(last, pairs(xs); init = Int32(0)) == sum(xs)
+    VERSION >= v"1.6-" || return
     @test Folds.sum(last, pairs(xs)) == sum(xs)
 end
 
