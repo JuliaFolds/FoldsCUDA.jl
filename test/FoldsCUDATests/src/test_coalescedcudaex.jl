6363bf6dcc4e0acd03dccaca8531d21b8e730222
Takafumi Arakaki <aka.tkf@gmail.com>
2021-07-19 19:09:43 -0400

    Skip CoalescedCUDAEx tests without init in Julia 1.5

--- test/FoldsCUDATests/src/test_coalescedcudaex.jl
+++ test/FoldsCUDATests/src/test_coalescedcudaex.jl
@@ -6,28 +6,27 @@ using Folds.Testing: parse_tests, test_with_sequential
 using FoldsCUDA
 using Transducers
 
+# Note: each line must ends with `; init = .*)`. See below.
 rawdata = """
 mapreduce(x -> x^2, +, 1:10; init = 0)
-mapreduce(x -> x^2, +, 1:10)
 mapreduce(*, +, 1:10, 11:20; init = 0)
-mapreduce(*, +, 1:10, 11:20)
 mapreduce(*, +, 1:10, 11:20, 21:30; init = 0)
-mapreduce(*, +, 1:10, 11:20, 21:30)
 sum(1:10; init = 0)
-sum(1:10)
 sum(first, pairs(1:10); init = 0)
-sum(first, pairs(1:10))
 sum(last, pairs(1:10); init = 0)
-sum(last, pairs(1:10))
 sum(x^2 for x in 1:11; init = 0)
-sum(x^2 for x in 1:11)
 sum(x^2 for x in 1:11 if isodd(x); init = 0)
-sum(x^2 for x in 1:11 if isodd(x))
 sum(y for x in 1:11 if isodd(x) for y in 1:x:x^2; init = 0)
-sum(y for x in 1:11 if isodd(x) for y in 1:x:x^2)
 """
 
 tests = parse_tests(rawdata, @__MODULE__)
+
+if VERSION >= v"1.6"
+    lines = map(eachline(IOBuffer(rawdata))) do ln
+        replace(ln, r"; init = .*?\)$" => ")")
+    end
+    tests = append!(tests, parse_tests(join(lines, "\n"), @__MODULE__))
+end
 
 upload(x) = adapt(CuArray, x)
 
