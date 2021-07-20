bc5a1034c219361371258fe5a5b1175af57b6cab
Takafumi Arakaki <aka.tkf@gmail.com>
2021-07-20 01:15:15 -0400

    Exclude large-input test in Julia 1.5

--- test/FoldsCUDATests/src/test_coalescedcudaex.jl
+++ test/FoldsCUDATests/src/test_coalescedcudaex.jl
@@ -12,7 +12,7 @@ mapreduce(x -> x^2, +, 1:10; init = 0)
 mapreduce(*, +, 1:10, 11:20; init = 0)
 mapreduce(*, +, 1:10, 11:20, 21:30; init = 0)
 sum(1:10; init = 0)
-sum(1:2^30; init = 0)
+sum(1:2^30; init = 0)                                                   # nojl15
 sum(first, pairs(1:10); init = 0)
 sum(last, pairs(1:10); init = 0)
 sum(x^2 for x in 1:11; init = 0)
@@ -21,6 +21,10 @@ sum(y for x in 1:11 if isodd(x) for y in 1:x:x^2; init = 0)
 """
 
 tests = parse_tests(rawdata, @__MODULE__)
+
+if VERSION < v"1.6"
+    filter!(t -> :nojl15 ∉ t.tags, tests)
+end
 
 if VERSION >= v"1.6"
     lines = map(eachline(IOBuffer(rawdata))) do ln
