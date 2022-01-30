83d6fab5d19e3a3b061854208066420573f02431
Takafumi Arakaki <aka.tkf@gmail.com>
2022-01-29 22:50:46 -0800

    Use TestFunctionRunner (#87)

--- test/FoldsCUDATests/src/utils.jl
+++ test/FoldsCUDATests/src/utils.jl
@@ -2,6 +2,18 @@ module Utils
 
 using CUDA
 
+function include_tests(m, dir = @__DIR__)
+    for file in readdir(dir)
+        if match(r"^test_.*\.jl$", file) !== nothing
+            Base.include(m, joinpath(dir, file))
+        end
+    end
+end
+
+should_test_gpu() =
+    lowercase(get(ENV, "JULIA_PKGEVAL", "false")) != "true" &&
+    lowercase(get(ENV, "CUDAFOLDS_JL_TEST_GPU", "true")) == "true"
+
 """
     unsafe_free_all!(namespace::Module)
 
